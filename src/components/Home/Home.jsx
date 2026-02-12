29bf027407950a8fab96974ff780f26081f76e24
GhitaDennoune <dennouneghita@example.com>
2026-02-12 13:30:18 +0100

    section des images Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,111 @@
 import Footer from "../Footer/Footer"
+import { Image } from "../../constant/images";
 
 function Home() {
   return (
     <>
-    
-    
-    <div>
-      <h2>Welcome to the Movie App!</h2>
-      <p>Discover and explore your favorite movies.</p>
+    <section>
+{/* caroussel */}
+{/* les images section  */}
+
+<div className="  w-[1200px]  mx-auto h-[800px]  flex gap-10">
+
+<div className="w-[365px]   flex flex-col gap-10  relative overflow-hidden  h-full ">
+  <div  className="relative  overflow-hidden">
+<img src={Image.model1} alt="" className=" object-cover   transition-transform duration-500 ease-in-out hover:scale-120" />
+<div className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-white/90 px-10 py-3">
+      <h2 className="text-gray-700 tracking-widest text-lg font-medium">
+        DRESSES
+      </h2>
+    </div>
+  </div>
+
+
+
+
+
+<img src={Image.card2} className="w-full h-[280px]   object-cover   transition-transform duration-500 ease-in-out hover:scale-120  " alt=""  />
+
+
+ <div className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-white/90 px-10 py-3">
+      <h2 className="text-gray-700 tracking-widest text-lg font-medium">
+        SUNGLASSES
+      </h2>
+    </div>
+</div>
+
+
+
+<div className="w-[365px]    flex flex-col gap-9  h-full  ">
+<div className="relative overflow-hidden">
+<img src={Image.item1} alt="" className="w-full h-[330px] object-cover   transition-transform duration-500 ease-in-out hover:scale-120" />
+<div className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-white/90 px-10 py-3">
+      <h2 className="text-gray-700 tracking-widest text-lg font-medium">
+        WATCHES
+      </h2>
     </div>
 
+</div>
+
+<div className="relative  overflow-hidden">
+<img src={Image.card4} alt=""  className="  w-full   h-[430px] object-cover   transition-transform duration-500 ease-in-out hover:scale-120"/>
+
+<div className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-white/90 px-10 py-3">
+      <h2 className="text-gray-700 tracking-widest text-lg font-medium">
+        FOOTERWEAR
+      </h2>
+    </div>
+
+
+</div>
+</div>
+<div className="w-[365px]   flex-col gap-7  h-full">
+
+  <div className="relative overflow-hidden">
+<img src={Image.card1} alt="" className="object-cover   transition-transform duration-500 ease-in-out hover:scale-120" />
+<div className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-white/90 px-10 py-3">
+      <h2 className="text-gray-700 tracking-widest text-lg font-medium">
+        BAGS
+      </h2>
+    </div>
+
+  </div>
+
+  <div className="relative overflow-hidden ">
+<img src={Image.card3} alt="" className=" w-full h-[295px] object-cover   transition-transform duration-500 ease-in-out hover:scale-120 " />
+
+<div className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-white/90 px-10 py-3">
+      <h2 className="text-gray-700 tracking-widest text-lg font-medium">
+        ACCESSORIES
+      </h2>
+    </div>
+
+  </div>
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+    </section>
+  
+
 <div>
+
+
+
+
+  
 <Footer/>
 </div>
     </>
--- src/constant/images.jsx
+++ src/constant/images.jsx
@@ -2,6 +2,14 @@ import reactsvg from '../assets/react.svg';
 import logo from "../assets/images (1)/images/logo.png.webp"
 import shopy from "../assets/images (1)/images/shopy4.webp"
 import About from "../assets/images (1)/images/banner.webp"
+import model1 from "../assets/images (1)/images/cards1.webp"
+import item1 from "../assets/images (1)/images/item-05.jpg"
+import card1 from "../assets/images (1)/images/cards3.webp"
+import card2 from "../assets/images (1)/images/cards4.webp"
+import card3 from "../assets/images (1)/images/cards6.webp"
+import card4 from "../assets/images (1)/images/cards5.webp"
+
+
 export const Image = {
- reactsvg,logo,shopy,About
+ reactsvg,logo,shopy,About,model1,card1,card3,card4,item1,card2
 }
